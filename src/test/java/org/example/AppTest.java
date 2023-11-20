c80db7cd51f5762386d57d1c3371365e0419c6d7
renjunhao <3370461908@qq.com>
2023-11-20 20:57:43 +0800

    初始化1

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -16,5 +16,8 @@ public class AppTest
     public void testabc(){
         System.out.println("abc....");
         System.out.println("abc....");
+        System.out.println("abc....");
+        System.out.println("abc....");
+        System.out.println("abc....");
     }
 }
